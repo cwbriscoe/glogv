0d43d83b3f7058be5b5a1b66765c9392db5f2a56
cwbriscoe <cbriscoe658@yahoo.com>
2022-08-10 16:50:47 -0700

    small refactor

--- glogv.go
+++ glogv.go
@@ -10,6 +10,11 @@ import (
 	"github.com/goccy/go-json"
 )
 
+const (
+	maxKeys       = 100 // maximum size json key slice.
+	errorExitCode = 4   // exit code if error occurs.
+)
+
 // ANSI color escape codes
 var (
 	colorReset  = "\033[0m"
@@ -23,80 +28,77 @@ var (
 	colorWhite  = "\033[37m"
 )
 
-// this struct will be used to marshall the json file into key/values
-type keyvalues struct {
-	Map map[string]interface{} `json:"-"`
+// this struct will be used to marshall the json file into key/values.
+type keyValues struct {
+	Map map[string]any `json:"-"`
 }
 
-// this string slice will store keys and then sort them
-var maxKeys = 100
+// this string slice will store keys and then sort them.
 var keys = make([]string, 0, maxKeys)
 
-const errorExitCode = 4
-
 func main() {
-	// create a stdin scanner and loop until all log lines are processed
+	// create a stdin scanner and loop until all log lines are processed.
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
-		if err := reformat(scanner.Bytes()); err != nil {
-			fmt.Println(err)
-			os.Exit(errorExitCode)
-		}
+		reformat(scanner.Bytes())
 	}
 
 	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
+		fmt.Printf("scanner error: %v\n", err)
 		os.Exit(errorExitCode)
 	}
 }
 
 // reformats the json log line into a prettier, more readable version.
-func reformat(b []byte) error {
+func reformat(b []byte) {
 	var tm time.Time
 	var level, message, errorx string
 
-	// marshall the current log entry into a key/value map
-	keyvals := &keyvalues{}
-	if err := json.Unmarshal(b, &keyvals.Map); err != nil {
-		return err
+	// first make sure the log line is json, if not return without processing.
+	if string(b[:1]) != "{" {
+		return
 	}
 
-	// first parse and format the standard logging fields
-	if val, ok := keyvals.Map["time"]; ok {
+	// marshall the current log entry into a key/value map.
+	keyVals := &keyValues{}
+	if err := json.Unmarshal(b, &keyVals.Map); err != nil {
+		return
+	}
+
+	// first parse and format the standard logging fields.
+	if val, ok := keyVals.Map["time"]; ok {
 		tm, _ = time.Parse(time.RFC3339, val.(string))
 	}
-	if val, ok := keyvals.Map["level"]; ok {
+	if val, ok := keyVals.Map["level"]; ok {
 		level = val.(string)
 	}
-	if val, ok := keyvals.Map["message"]; ok {
+	if val, ok := keyVals.Map["message"]; ok {
 		message = val.(string)
 	}
-	if val, ok := keyvals.Map["error"]; ok {
+	if val, ok := keyVals.Map["error"]; ok {
 		errorx = val.(string)
 	}
 
-	// reformat what we have parsed so far
+	// reformat what we have parsed so far.
 	tmStr := formatTime(tm)
 	lvlStr := formatLevel(level)
 	msgStr := formatMessage(message, level)
 	errStr := formatError(errorx)
 
-	// next delete the keys we just processed from the map
-	delete(keyvals.Map, "time")
-	delete(keyvals.Map, "level")
-	delete(keyvals.Map, "message")
-	delete(keyvals.Map, "error")
+	// next delete the keys we just processed from the map.
+	delete(keyVals.Map, "time")
+	delete(keyVals.Map, "level")
+	delete(keyVals.Map, "message")
+	delete(keyVals.Map, "error")
 
-	// now, parse through the remaining key/values in the map
-	valStr := formatMap(keyvals.Map)
+	// now, parse through the remaining key/values in the map.
+	valStr := formatMap(keyVals.Map)
 
-	// finally, print the prettier log entry
+	// finally, print the prettier log entry.
 	fmt.Printf("%s%s%s%s%s\n", tmStr, lvlStr, msgStr, errStr, valStr)
-
-	return nil
 }
 
-// formats the 'time' portion of the json log line
+// formats the 'time' portion of the json log line.
 func formatTime(t time.Time) string {
 	s := t.Format(time.Kitchen)
 
@@ -107,7 +109,7 @@ func formatTime(t time.Time) string {
 	return colorGray + s
 }
 
-// formats the 'level' portion of the json log line
+// formats the 'level' portion of the json log line.
 func formatLevel(s string) string {
 	switch s {
 	case "info":
@@ -125,7 +127,7 @@ func formatLevel(s string) string {
 	}
 }
 
-// formats the 'message' portion of the json log line
+// formats the 'message' portion of the json log line.
 func formatMessage(s string, l string) string {
 	if s == "" {
 		return s
@@ -138,7 +140,7 @@ func formatMessage(s string, l string) string {
 	return " " + s
 }
 
-// formats the 'error' portion of the json log line
+// formats the 'error' portion of the json log line.
 func formatError(s string) string {
 	if s == "" {
 		return s
@@ -147,15 +149,15 @@ func formatError(s string) string {
 	return " (error: " + s + ")"
 }
 
-// formats the remaining key/value pairs of the json log line
-func formatMap(m map[string]interface{}) string {
+// formats the remaining key/value pairs of the json log line.
+func formatMap(m map[string]any) string {
 	l := len(m)
-	// if the map is empty then return nothing
+	// if the map is empty then return nothing.
 	if l == 0 {
 		return ""
 	}
 
-	// if there is just one value left in the map, return it now
+	// if there is just one value left in the map, return it now.
 	if l == 1 {
 		for k, v := range m {
 			return " " + colorGray + k + "=" + colorReset + v.(string)
@@ -163,7 +165,7 @@ func formatMap(m map[string]interface{}) string {
 	}
 
 	// there is more than 1 value in the map, so we will sort by
-	// key to get a consistent order
+	// key to get a consistent order.
 	keys = keys[:0]
 	i := 0
 	for k := range m {
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/cwbriscoe/glogv
 
-go 1.16
+go 1.18
 
 require github.com/goccy/go-json v0.9.10
